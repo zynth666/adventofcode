e7d39023482913a0c9dfcbb80c93b8a021adb692
Dennis Eisenfeld <dennis.eisenfeld@yahoo.de>
2025-12-06 00:25:23 +0100

    map out solution and make first steps

--- 2025/day-1/main.c
+++ 2025/day-1/main.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
-
+/**
+ * https://adventofcode.com/2025/day/1
+ * 
+ * calculate number of times dial lands exactly on 0
+ * write a clamping function that subtracts 100 from currentValue if it goes above 100
+ * i.e. currentValue = 95; input is R6 -> 101 - 100 = 1
+ * and for negative values do 100 + -currentValue i.e 100 - 1 = 99
+ * 
+ * landing directly on 100 should be treated as 0
+ */
 int main() {
-    printf("Hello world!\n");
+    FILE *file = fopen("input.txt", "r");
+
+    if (file == NULL) {
+        perror("Error opening file");
+        return 1;
+    }
+
+    int lineBufferLength = 10;
+    char lineBuffer[lineBufferLength];
+
+    while(fgets(lineBuffer, lineBufferLength, file)) {
+        puts(lineBuffer);
+    }
+
+    fclose(file);
+
     return 0;
 }
